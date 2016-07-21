0e6af7f55ad7b52a9ccbf9f1cb179ba91231d7d8
ezefib <esarmiento@thinkful.com>
2016-07-20 22:21:10 -0700

    Swift 3 - Unit 3.2.3 Complete

--- RGBSliders/ViewController.swift
+++ RGBSliders/ViewController.swift
@@ -10,16 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redSlider: UISlider!
+    
+    @IBOutlet weak var greenSlider: UISlider!
+    
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBOutlet weak var colorSquare: UIView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        colorSquare.layer.borderColor = UIColor.black().cgColor
+        colorSquare.layer.borderWidth = 1
+        
+        
+        updateBackgroundColor()
     
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func updateBackgroundColor() {
+        
+        let red = CGFloat(redSlider.value)
+        let green = CGFloat(greenSlider.value)
+        let blue = CGFloat(blueSlider.value)
+        
+        
+        //view.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        colorSquare.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
     }
 
 
